70f5c4d0132d50e3a446af287eb0549085afddff
jiaderle <qq1011221325@163.com>
2026-03-24 15:00:03 +0800

    创建更新用户资料后端api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -6,6 +6,7 @@ from web.views.user.account.login import LoginView
 from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
+from web.views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -15,6 +16,7 @@ urlpatterns = [
     path('api/user/account/register/', RegisterView.as_view()),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
+    path('api/user/profile/update/', UpdateProfileView.as_view()),
     path('', index),
 
     re_path(r'^(?!media/|static/|assets/).*$', index),
