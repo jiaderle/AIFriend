9ce3cdda36c54a894381e511f2ca3e95339efa49
jiaderle <qq1011221325@163.com>
2026-04-12 20:43:33 +0800

    创建消息数据库

--- backend/web/models/friend.py
+++ backend/web/models/friend.py
@@ -4,7 +4,6 @@ from django.utils.timezone import now, localtime
 from web.models.character import Character
 from web.models.user import UserProfile
 
-# 用户与虚拟角色的好友关系
 class Friend(models.Model):
     me = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
@@ -14,3 +13,17 @@ class Friend(models.Model):
 
     def __str__(self):
         return f"{self.character.name} - {self.me.user.username} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class Message(models.Model):
+    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+    user_message = models.TextField(max_length=500)
+    input = models.TextField(max_length=500)
+    output = models.TextField(max_length=500)
+    input_tokens = models.IntegerField(default=0)
+    output_tokens = models.IntegerField(default=0)
+    total_tokens = models.IntegerField(default=0)
+    create_time = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.friend.character.name} - {self.friend.me.user.username} - {self.user_message[:50]} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
