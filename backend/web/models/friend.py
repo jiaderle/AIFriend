2c9205534299d6a232038fda436529e3e3723a6b
jiaderle <qq1011221325@163.com>
2026-04-14 15:10:08 +0800

    添加系统提示词以及多轮对话

--- backend/web/models/friend.py
+++ backend/web/models/friend.py
@@ -4,6 +4,7 @@ from django.utils.timezone import now, localtime
 from web.models.character import Character
 from web.models.user import UserProfile
 
+
 class Friend(models.Model):
     me = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
@@ -27,3 +28,14 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.friend.character.name} - {self.friend.me.user.username} - {self.user_message[:50]} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class SystemPrompt(models.Model):
+    title = models.CharField(max_length=100)
+    order_number = models.IntegerField(default=0)
+    prompt = models.TextField(max_length=10000)
+    create_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.title} - {self.order_number} - {self.prompt[:50]} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
