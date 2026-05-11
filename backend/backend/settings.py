bb5f88badf5170d085c6478bbb49d6a478058bc5
jiaderle <qq1011221325@163.com>
2026-05-11 11:19:01 +0800

    统一管理项目常量

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'django-insecure-s1ag(z61nl56pb21$un)wssmbm2mdwqf+c3y^ovq2ygs^_n70g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'app8036.acapp.acwing.com.cn']
 
 # Application definition
 
@@ -123,13 +122,19 @@ USE_TZ = True
 
 # 设置static和media静态文件路径
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'  # 生产阶段使用
 
-STATICFILES_DIRS = [  # 开发阶段使用，生产阶段需要注释掉
-    BASE_DIR / 'static',
-]
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'  # 生产阶段使用
+else:
+    STATICFILES_DIRS = [  # 开发阶段使用，生产阶段需要注释掉
+        BASE_DIR / 'static',
+    ]
 
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+if DEBUG:
+    MEDIA_URL = 'http://127.0.0.1:8000/media/'
+else:
+    MEDIA_URL = 'https://app8036.acapp.acwing.com.cn/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
